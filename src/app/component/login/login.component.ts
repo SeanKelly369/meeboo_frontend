f69a6d77dc019dc744afbd150342a6e159efd440
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-12-04 13:50:53 +0000

    Add files via upload

---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(8)]) // password match
+    });
+   }
+   
+
+  result: any;
+
+  ngOnInit(): void {
+  }
+
+  onClickSubmit() {
+    console.log('reun'); 
+ }
+
+}
